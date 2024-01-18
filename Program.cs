b666c82292ecfc15bd8ad369ac4ec94f705afbb0
amorimfe7 <amorimfe7@gmail.com>
2024-01-17 21:43:50 -0300

    Iniciando o projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Calculator{
+
+    class Program{
+        static void Main(string[] args){
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+
+}
+
--- obj/Debug/net8.0/projCalculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/projCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ff254fd1c56104ac37ff57702a20174ae4d73e")]
 [assembly: System.Reflection.AssemblyProductAttribute("projCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
